811819ff745e89e8fa41acc88118b86988ff382a
Saiyann <knt4xyz@gmail.com>
2021-12-15 13:43:40 +0100

    📝toggle isActive / isNotActive class on Buttons A/B/C/D

--- src/components/stripe_zone/ButtonStripeMap.tsx
+++ src/components/stripe_zone/ButtonStripeMap.tsx
@@ -1,6 +1,48 @@
-import React from 'react'
+import React , { useState } from 'react'
 
 function ButtonStripeMap() {
+
+    const [ classBtn_A , setClassBtn_A ] = useState('isActive');
+    const [ classBtn_B , setClassBtn_B ] = useState('isNotActive');
+    const [ classBtn_C , setClassBtn_C ] = useState('isNotActive');
+    const [ classBtn_D , setClassBtn_D ] = useState('isNotActive');
+
+    const handleOnClick_A = (e: any) => {
+        e.preventDefault()
+        console.log('button A clicked')
+        
+        classBtn_A === 'isNotActive'? 
+            setClassBtn_A('isActive') : 
+            setClassBtn_A('isActive') ; setClassBtn_B('isNotActive') ; setClassBtn_C('isNotActive') ; setClassBtn_D('isNotActive')
+    }
+
+    const handleOnClick_B = (e: any) => {
+        e.preventDefault()
+        console.log('button B clicked')
+        
+        classBtn_B === 'isNotActive'? 
+            setClassBtn_B('isActive') : 
+            setClassBtn_B('isActive') ; setClassBtn_A('isNotActive') ; setClassBtn_C('isNotActive'); setClassBtn_D('isNotActive')
+    }
+    
+    const handleOnClick_C = (e: any) => {
+        e.preventDefault()
+        console.log('button C clicked')
+        
+        classBtn_C === 'isNotActive'? 
+            setClassBtn_C('isActive') : 
+            setClassBtn_C('isActive') ; setClassBtn_A('isNotActive') ; setClassBtn_B('isNotActive'); setClassBtn_D('isNotActive') 
+    }
+
+    const handleOnClick_D = (e: any) => {
+        e.preventDefault()
+        console.log('button C clicked')
+        
+        classBtn_C === 'isNotActive'? 
+            setClassBtn_D('isActive') : 
+            setClassBtn_D('isActive') ; setClassBtn_A('isNotActive') ; setClassBtn_B('isNotActive') ; setClassBtn_C('isNotActive') 
+    }
+
     return (
         <div className='ButtonStripeMap'
             style={{
@@ -10,10 +52,22 @@ function ButtonStripeMap() {
                 padding: '15px',
             }}
         >
-            <div className='btn'>A</div>
-            <div className='btn'>B</div>
-            <div className='btn'>C</div>
-            <div className='btn'>D</div>
+            <button 
+                className={`btn ${classBtn_A}`} 
+                onClick={handleOnClick_A}
+            >A</button>
+            <button className={`btn ${classBtn_B}`}
+                onClick={handleOnClick_B}
+            >B
+            </button>
+            <button className={`btn ${classBtn_C}`}
+                onClick={handleOnClick_C}
+            >C
+            </button>
+            <button className={`btn ${classBtn_D}`}
+                onClick={handleOnClick_D}
+            >D
+            </button>
         </div>
     )
 }
